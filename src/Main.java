b0bcd5fd9ce4a84359b1a6ae80688f7561ceb8b7
Andre3243 <sebastiancufi2@gmail.com>
2026-01-17 02:34:32 -0500

    intial app in java whit simple sintax, using tex-box and the format method

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+       String nombre  = "burrito";
+        System.out.println(String.format("Bienvenido %s a Screen Match, tu cine de confianza",nombre));
+// mensaje de bienvenida, usamos el metodo .format para poder agregar el string con el nombre.
+        System.out.println("Pelicula: Matrix ");
+
+
+        int fechaDelanzamiento = 1999;
+        boolean incluideInPlan = true;
+        double  notaDelaPelicula = 8.2;
+
+        double media =  (8.2 + 6.0 + 9.0) / 3;
+        System.out.println( media);
+
+        String sinopsis = """
+                Matrix es una paradoja 
+                La mejor pelicula del fin del milenio 
+                Fue lanzada en: %s , teniendo esta una nota de %.1f
+                """         .formatted(fechaDelanzamiento,notaDelaPelicula);
+        System.out.println(sinopsis);
+    }
+}
+
+
